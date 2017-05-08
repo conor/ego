373b5af9dc15e546e3fa5131c5f1a11f38408318
Conor Wade <conorwade@gmail.com>
2017-05-08 21:46:49 +0100

    Improved footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
+// Theme
+import {
+  spacingLarge,
+  spacingExtraLarge,
+  spacingExtraExtraLarge
+} from '../theme/spacing'
+import { f6, bold } from '../theme/typography'
+import { brandBlue, brandWhite } from '../theme/colors'
+import { largeAndUp } from '../theme/screenSizes'
+
 const Footer = () => (
   <footer>
     <p>© 2017 CONOR WADE. ALL RIGHTS RESERVED</p>
@@ -9,6 +19,42 @@ const Footer = () => (
       {' '}
       <a href='https://www.google.com/search?q=vancouver'>Vancouver, Canada</a>
     </p>
+    <style jsx>{`
+      footer {
+        padding: ${spacingExtraLarge} ${spacingLarge};
+        display: flex;
+        flex-wrap: wrap;
+        text-align: center;
+        justify-content: space-between;
+      }
+      p {
+        flex-basis: 100%;
+        font-size: ${f6};
+        font-weight: ${bold};
+        text-transform: uppercase;
+      }
+      img {
+        flex-basis: 100%;
+      }
+      a {
+        color: ${brandBlue};
+        text-decoration: none;
+        border-bottom: 2px solid ${brandBlue};
+        margin: 0 2px;
+        transition: border-bottom-color 0.3s ease-in;
+      }
+      a:hover {
+        border-bottom-color: ${brandWhite};
+      }
+      @media ${largeAndUp} {
+        footer {
+          padding: ${spacingExtraExtraLarge} ${spacingExtraLarge};
+        }
+        p, img {
+          flex-basis: auto;
+        }
+      }
+    `}</style>
   </footer>
 )
 
