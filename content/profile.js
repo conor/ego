4887665df1d3d7df4a8bcd9936face67463386e9
Conor Wade <conorwade@gmail.com>
2017-05-10 13:49:38 +0100

    Improve bio copy

--- content/profile.js
+++ content/profile.js
@@ -3,11 +3,10 @@ const Profile = {
   picture: '/static/profile.jpg',
   pictureAlt: 'Picture of Conor Wade, the author of the site.',
   bio: `
-    <p>I’m a designer / developer who loves to travel. I have 9 year’s experience mostly in the startup world. I believe in strong cross-polination between design & adevelopment. Nothing makes a team more productive than designers and developers working closely to push a product forward.</p>
-    <p>My passion is at the intersection of design & technology, and how we can use these disciplines to solve problems for all kinds of organization in all kinds of disciplines.</p>
-    <p>I have had the good fortune to lead both design & technology on different occasions, and I have co-founded one startup in my career, <a href="http://www.chronogolf.ca">Chronogolf</a>.</p>
-    <p>For the last few years, I have been working with the fantastic team at <a href="http://www.makewellmade.com">Makewell</a>, a full service design & development agency based out of Brooklyn, New York.</p>
-    <p>At the moment, I am experimenting with functional programming & atomic design / css. Seeing how far I can push prototyping & maintainable design / development.</p>
+    <p>I’m a designer / developer who loves to travel. I have 9 years experience mostly in the start-up world. I have co-founded the market leading Golf Club management <a href="https://chronogolf.ca">platform (Chronogolf)</a> in Canada. Down through the years I have written software in <a href="https://www.ruby-lang.org/">Ruby</a>, <a href="https://www.javascript.com/">JS</a>, and Coffeescript. In the last few years, I have begun to explore the world of functional programming. Namely, <a href="https://clojure.org/">Clojure(script)</a>, and more recently <a href="http://elixir-lang.org/">Elixir</a> and <a href="http://elm-lang.org/">Elm</a>, whilst trying to bring some of those techniques back to JS, with the help of <a href="https://facebook.github.io/react/">React</a>, and tools such ImmutableJS, and Rambda.</p>
+    <p>I have had the good fortune to lead both design & technology on multiple occasions. Working at the intersection of design & technology has lead me to value prototyping and the use of design sprints to get early feedback from users. I believe strongly in the use of design systems in order to implement a consistent and coherent experience across a product or brand.</p>
+    <p>For the last few years, I have been working with the fantastic team at <a href="https://www.makewellmade.com">Makewell</a>, a full service design agency based out of Brooklyn, New York.</p>
+    <p>A few years ago my wife, and I started to travel the world as Digital Nomads. During that time we have had the chance to explore some of the most amazing parts of the world. All the while experiencing other culture's, and learning some of the intricacies that prevail in other culture's and my own. I have included a map showing where we are now, and where we have been.</p>
   `
 }
 
